93b4d637e5a2a3d13a6d929190acb88206d9bf13
Jan De Poorter <jan@defv.be>
2009-09-30 17:21:17 +0200

    Monkey patch rails so the following scenario works:
    class Report
      has_and_belongs_to_many :people
      has_many :contact_details, :through => :people
    end

--- lib/has_many_through_habtm.rb
+++ lib/has_many_through_habtm.rb
@@ -15,6 +15,20 @@ module HasManyThroughHabtm
   module HasManyThroughAssociation
     def self.included(base)
       base.alias_method_chain :construct_joins, :habtm
+      base.alias_method_chain :construct_conditions, :habtm
+    end
+
+    def construct_conditions_with_habtm
+      if @reflection.through_reflection.macro == :has_and_belongs_to_many
+        table_name = @reflection.through_reflection.options[:join_table]
+        conditions = construct_quoted_owner_attributes(@reflection.through_reflection).map do |attr, value|
+          "#{table_name}.#{attr} = #{value}"
+        end
+        conditions << sql_conditions if sql_conditions
+        "(" + conditions.join(') AND (') + ")"
+      else
+        construct_conditions_without_habtm
+      end
     end
 
     def construct_joins_with_habtm(custom_joins = nil)
@@ -27,6 +41,12 @@ module HasManyThroughHabtm
           @reflection.through_reflection.quoted_table_name,
           @reflection.source_reflection.options[:join_table], @reflection.source_reflection.primary_key_name,
           @reflection.through_reflection.quoted_table_name, @reflection.klass.primary_key
+        ]
+      elsif @reflection.through_reflection.macro == :has_and_belongs_to_many
+        construct_joins_without_habtm(custom_joins) + " INNER JOIN %s ON %s.%s = %s.%s" % [
+          @reflection.through_reflection.options[:join_table].inspect,
+          @reflection.through_reflection.options[:join_table].inspect, @reflection.through_reflection.association_foreign_key,
+          @reflection.through_reflection.quoted_table_name, @reflection.through_reflection.klass.primary_key
         ]
       else
         construct_joins_without_habtm(custom_joins)
